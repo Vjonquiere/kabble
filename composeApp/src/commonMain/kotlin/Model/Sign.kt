8855297c688c1eb773790cd90993f844def2d98d
Denis Demirci <denis.demirci@etu.u-bordeaux.fr>
2023-12-07 20:00:48 +0100

    class Sign + getId and setId

---
+++ composeApp/src/commonMain/kotlin/Model/Sign.kt
@@ -0,0 +1,17 @@
+package Model
+
+class Sign(var id: Int = -1) {
+
+
+
+
+    fun getId(sign: Sign): Int {
+        return this.id
+    }
+
+    fun setId(sign: Sign, newId : Int){
+        this.id = newId
+    }
+
+
+}
